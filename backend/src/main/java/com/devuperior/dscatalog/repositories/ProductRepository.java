b176651b60da58edf0ea552000d0e89e77452e01
Fernando Lopes dos Santos <store.nthing@gmail.com>
2021-10-07 21:33:18 -0300

    Product repository, service, resource, GET ok

---
+++ backend/src/main/java/com/devuperior/dscatalog/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.devuperior.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devuperior.dscatalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
